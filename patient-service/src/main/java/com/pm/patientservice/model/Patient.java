3c389fb3768a63914ee5a4055c1b91f7c3fd086e
Prabal-pratik-singh <mannkumarsinghprabal1@gmail.com>
2026-06-12 06:55:56 +0530

    made endpoint to get all patient information R-retrive

--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-public class patient {
+public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO )
     private UUID id;
